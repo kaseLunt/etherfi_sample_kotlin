7da23cac39f6980315d6e8276e52ee32fd62b985
kasel <kllunt@asu.edu>
2025-11-10 10:32:31 -0700

    linting

--- app/src/main/java/com/babel/etherfiportfoliotracker/ui/screens/SimulatedStakingScreen.kt
+++ app/src/main/java/com/babel/etherfiportfoliotracker/ui/screens/SimulatedStakingScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.babel.etherfiportfoliotracker.R
-import com.babel.etherfiportfoliotracker.ui.screens.shared.CardBackground
 import com.babel.etherfiportfoliotracker.ui.screens.shared.HeaderBackground
 import com.babel.etherfiportfoliotracker.ui.screens.shared.LavenderText
 import com.babel.etherfiportfoliotracker.ui.screens.shared.PurpleArrow
